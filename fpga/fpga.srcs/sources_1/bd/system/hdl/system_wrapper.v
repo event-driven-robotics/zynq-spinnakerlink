15a21baee02629bc842cdefb605a2fbecb5852c9
mcasti-iit <maurizio.casti@iit.it>
2018-08-24 14:16:48 +0200

    Fixes a missing connection in block diagram

--- fpga/fpga.srcs/sources_1/bd/system/hdl/system_wrapper.v
+++ fpga/fpga.srcs/sources_1/bd/system/hdl/system_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2017.4.1 (win64) Build 2117270 Tue Jan 30 15:32:00 MST 2018
-//Date        : Thu Aug 23 15:24:33 2018
+//Date        : Fri Aug 24 11:18:15 2018
 //Host        : IITICUBWS052 running 64-bit major release  (build 9200)
 //Command     : generate_target system_wrapper.bd
 //Design      : system_wrapper
